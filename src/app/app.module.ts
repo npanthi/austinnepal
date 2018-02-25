29093fc79d961dc59f0278335c455610240c45d0
Nisan panthi <Nisan@nisans-mbp.attlocal.net>
2018-02-24 20:03:39 -0600

    hi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
-
+import * as $ from 'jquery';
 
 import {HttpClientModule} from '@angular/common/http'
 
